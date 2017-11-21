e0c8633565d77678059a01e7e9a9e868b438142c
Tim Weber <scy@scy.name>
2017-11-21 15:14:48 +0100

    Implement changing the switch IP address

--- infra/switch/setup-switch.py
+++ infra/switch/setup-switch.py
@@ -3,6 +3,7 @@ import pexpect, subprocess, sys
 # There's a server that does NTP, routing, DNS etc.
 SERVER_IP = '10.10.1.254'
 NTP_SERVER = SERVER_IP
+DEF_GATEWAY = SERVER_IP
 
 HOSTNAME = 'tabr' # How the switch should be called.
 
@@ -11,11 +12,14 @@ USER = 'admin'
 PASS = USER
 # The default IP of the switch.
 SWITCH_INITIAL_IP = '192.168.0.1'
+# The IP it should have when we're done configuring it.
+SWITCH_IP = '10.10.1.250'
 
 PROMPT_HOST_REGEX = '(TL-SG2008|{0})'.format(HOSTNAME)
 USER_PROMPT = PROMPT_HOST_REGEX + '>'
 PRIV_PROMPT = PROMPT_HOST_REGEX + '#'
 GLOB_PROMPT = PROMPT_HOST_REGEX + '\(config\)#'
+MGMT_VLAN_PROMPT = PROMPT_HOST_REGEX + '\(config-if\)#'
 
 '''Send and expect a prompt.'''
 def sne(self, to_send, expect=None):
@@ -29,21 +33,40 @@ def sne(self, to_send, expect=None):
 # Enhance Pexpect with our sne method.
 pexpect.spawn.sne = sne
 
-t = pexpect.spawn('telnet', ['-E', SWITCH_INITIAL_IP], timeout=5, encoding='ascii')
-t.logfile_read = sys.stdout # Echo what we receive.
-t.linesep = '\r\n' # Sending linefeeds only is not supported by the switch.
+def connect_and_config(ip, user, pw):
+    # Connect and configure some settings.
+    t = pexpect.spawn('telnet', ['-E', ip], timeout=5, encoding='ascii')
+    t.logfile_read = sys.stdout # Echo what we receive.
+    t.linesep = '\r\n' # Sending linefeeds only is not supported by the switch.
+    # Login.
+    match = t.expect([pexpect.EOF, 'User:'])
+    if match == 0:
+        return None
+    t.sne(user, 'Password:')
+    t.sne(pw, USER_PROMPT)
+    t.sne(t.linesep)
+    # Change to privileged mode.
+    t.sne('enable', PRIV_PROMPT)
+    # Change to global configuration mode.
+    t.sne('configure', GLOB_PROMPT)
+    return t
 
-# Login.
-t.expect('User:')
-t.sne(USER, 'Password:')
-t.sne(PASS, USER_PROMPT)
-t.sne(t.linesep)
+# Connect to the initial IP.
+t = connect_and_config(SWITCH_INITIAL_IP, USER, PASS)
 
-# Change to privileged mode.
-t.sne('enable', PRIV_PROMPT)
+if t is None:
+    print('Could not connect to the initial IP. This is not necessarily an error; it might already have the correct IP. Trying that.')
+else:
+    # Set management VLAN and IP address of the switch.
+    t.sne('ip management-vlan 1')
+    t.sne('interface vlan 1', MGMT_VLAN_PROMPT)
+    t.sendline('ip address {0} 255.255.255.0 {1}'.format(SWITCH_IP, DEF_GATEWAY)) # Expecting is useless, the IP has changed.
+    # Close the connection.
+    t.close()
+    t.wait()
 
-# Change to global configuration mode.
-t.sne('configure', GLOB_PROMPT)
+# Connect to the correct IP.
+t = connect_and_config(SWITCH_IP, USER, PASS)
 
 # Set basic information.
 t.sne('hostname tabr')

e8260ece3a81634f3dc4767c06e64ee1783d2d57
Tim Weber <scy@scy.name>
2020-12-31 14:42:46 +0000

    Phlox: Create a class for Raspberry Pi management

--- src/phlox/src/phlox.py
+++ src/phlox/src/phlox.py
@@ -6,16 +6,24 @@ import time
 
 class Phlox:
 
-    def __init__(self, pi_set_pin=5, pi_unset_pin=33,
+    def __init__(self,
+                 pi_set_pin=5, pi_unset_pin=33,
+                 pi_status_pin=25, pi_status_inv=False,
+                 pi_shutdown_pin=27, pi_shutdown_inv=True,
                  votronic_port=2,
                  votronic_de_pin=4, votronic_de_inv=False,
                  votronic_re_pin=2, votronic_re_inv=True):
         # We're using the Perthensis scheduler for multitasking.
         self.sch = sch = Scheduler()
 
-        # When booting, enable the Pi.
-        self.pi = LatchingRelay(pi_set_pin, pi_unset_pin)
-        self.pi.set()
+        # Raspberry Pi controller object.
+        self.pi = Pi(
+            Signal(pi_status_pin, Pin.IN, invert=pi_status_inv),
+            # Signal class inverts the constructor value if invert is True.
+            Signal(pi_shutdown_pin, Pin.OUT, invert=pi_shutdown_inv, value=0),
+            LatchingRelay(pi_set_pin, pi_unset_pin),
+        )
+        self.pi.power_on()  # Power it on when we boot.
 
         # Initialize Votronic RS485 interface via Arduino MKR 485 shield.
         self.votronic = microtonic.UART(
@@ -30,6 +38,20 @@ class Phlox:
         self.sch(Watchdog(60_000).watch)
 
         self.sch.run_forever()
+
+
+class Pi:
+
+    def __init__(self, status_sig, shutdown_sig, power_relay):
+        self.status_sig = status_sig
+        self.shutdown_sig = shutdown_sig
+        self.relay = power_relay
+
+    def is_powered(self):
+        return bool(self.status_sig)
+
+    def power_on(self):
+        self.relay.set()
 
 
 # TODO: Move to Perthensis.
